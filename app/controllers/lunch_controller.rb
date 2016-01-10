0320bd173d6f2f3a354c1745351792251a76c5c5
QuangRau <lethanhquang910@gmail.com>
2016-01-10 22:57:31 +0700

    fixed type format

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -2,21 +2,7 @@ class LunchController < ApplicationController
   before_action :check_request, only: :order
 
   def menu
-    response = {
-      "response_type": "ephemeral",
-      "text": "Here are the currently open tickets:",
-      "attachments": [
-        {
-          "text": "#123456 http://domain.com/ticket/123456 \n
-          #123457 http://domain.com/ticket/123457 \n
-          #123458 http://domain.com/ticket/123458 \n
-          #123459 http://domain.com/ticket/123459 \n
-          #123460 http://domain.com/ticket/123460"
-        }
-      ]
-    }
-    # render status: 200, json: Dish.today
-    render status: 200, json: response
+    render status: 200, json: Dish.today
   end
 
   def order
